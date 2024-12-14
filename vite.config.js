6e22749a88c56de2ec99c6d81c9f8d79e2e11082
Ajaynirm <ajay@AJ-MacBook-Air-M1.local>
2024-12-14 22:02:13 +0530

    final before production

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Directory for production files
+    sourcemap: false, // Disable source maps for smaller bundle size
+  },
+  server: {
+    port: 3000, // Development server port
+  },
+  base: '/', 
 })
